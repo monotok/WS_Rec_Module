703220a9ebaa6f003601dc7b21fd0f8dcacf60b9
Chris Hamer <developer@monotok.org>
2021-01-16 15:21:57 +0000

    update rec module to still run even if local bme280 is missing; useful for breadbord testing

--- src/LocalSensor.cpp
+++ src/LocalSensor.cpp
@@ -9,13 +9,13 @@ LocalSensor::LocalSensor(uint8_t cs_pin, uint8_t ledErrPin): bme(cs_pin), ledErr
     pinMode(ledErrPin, OUTPUT);
 }
 
-[[noreturn]] void LocalSensor::flashErrLed()
+void LocalSensor::flashErrLed()
 {
-    while (true) {
+    for (int i = 0; i<10; i++) {
         digitalWrite(ledErrPin, HIGH);
-        delay(50);
+        delay(100);
         digitalWrite(ledErrPin, LOW);
-        delay(50);
+        delay(100);
     }
 }
 
@@ -23,6 +23,7 @@ void LocalSensor::initialise()
 {
     if (!bme.begin()) {
         flashErrLed();
+        errInitialising = ERR_FOUND;
     }
 }
 
@@ -30,9 +31,11 @@ void LocalSensor::processTemperature(Sensor& sensor)
 {
     Sensor::SensorData* tmp = sensor.getTempSensorData();
     strcpy(tmp->sensorId, "1.0");
-    strcpy(tmp->sensorType, "tmp");
-    strcpy(tmp->unit, "c");
-    tmp->reading = bme.readTemperature();
+    if (errInitialising == ERR_NOT_FOUND) {
+        tmp->reading = bme.readTemperature();
+    } else {
+        tmp->reading = 0.0;
+    }
     sensor.addReceivedSensor();
     sensor.reinitialiseTempSensor();
 }
@@ -41,9 +44,11 @@ void LocalSensor::processHumidity(Sensor &sensor)
 {
     Sensor::SensorData* tmp = sensor.getTempSensorData();
     strcpy(tmp->sensorId, "1.1");
-    strcpy(tmp->sensorType, "hum");
-    strcpy(tmp->unit, "%");
-    tmp->reading = bme.readHumidity();
+    if (errInitialising == ERR_NOT_FOUND) {
+        tmp->reading = bme.readHumidity();
+    } else {
+        tmp->reading = 0.0;
+    }
     sensor.addReceivedSensor();
     sensor.reinitialiseTempSensor();
 }
@@ -52,9 +57,11 @@ void LocalSensor::processPressure(Sensor &sensor)
 {
     Sensor::SensorData* tmp = sensor.getTempSensorData();
     strcpy(tmp->sensorId, "1.2");
-    strcpy(tmp->sensorType, "pre");
-    strcpy(tmp->unit, "hPa");
-    tmp->reading = bme.readPressure() / 100.0F;
+    if (errInitialising == ERR_NOT_FOUND) {
+        tmp->reading = bme.readPressure() / 100.0F;
+    } else {
+        tmp->reading = 0.0;
+    }
     sensor.addReceivedSensor();
     sensor.reinitialiseTempSensor();
 }
